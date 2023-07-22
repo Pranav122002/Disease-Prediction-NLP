a4066a5cde7233264e255bbf996c4857b52cfba5
Pranav Patil <pranavpatil122002@gmail.com>
2023-07-23 01:00:47 +0530

    dataset-zip, readme update

--- README.md
+++ README.md
@@ -1,12 +1,39 @@
 # Disease-Prediction-NLP
 Disease prediction based on symptoms provided in a single text input using Natural Language Processing (NLP).
 
+## Demo
+* https://disease-prediction-nlp.streamlit.app/
+
+## Description
+
+The **PassiveAggressiveClassifier** is a popular algorithm for online learning tasks and is often used for binary text classification problems. It is a type of linear classifier, suitable for large-scale learning. The PassiveAggressiveClassifier model, is trained on the TF-IDF transformed training data.
+
+Term Frequency-Inverse Document Frequency (TF-IDF). **TfidfVectorizer** is used to convert the text data into numerical features.
+
+**Dataset** includes 884 diseases, but the model is trained only on 11 most common diseases as to increase the accuracy and as this diseases contains highest instances in the dataset.
+
+Diseases on which model is trained are :
+- Birth Control          
+- Depression              
+- Anxiety                 
+- Acne                    
+- Insomnia                
+- Diabetes, Type 2        
+- High Blood Pressure     
+- Migraine                
+- Constipation            
+- Cough                    
+- GERD                     
+
 ## Installation & Running
-
+1. Install the required packages:
 ```
-pip install streamlit numpy pandas scikit-learn nltk matplotlib seaborn pickle
+pip install streamlit numpy pandas scikit-learn nltk matplotlib
 ```
 
+2. Compile and save the models by running the entire disease_prediction.ipynb file.
+
+3. Run the Streamlit app:
 ```
 streamlit run app.py
 ```
--- app.py
+++ app.py
@@ -2,28 +2,67 @@ import numpy as np
 import pickle
 import pandas as pd
 import streamlit as st
+import zipfile
+import os
+
+
+def unzip_file(zip_path, extract_to):
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(extract_to)
+
+
+zip_file_path = "dataset-zip.zip"
+extraction_path = os.getcwd()
+unzip_file(zip_file_path, extraction_path)
 
 model = pickle.load(open("models/tfidf_trigrams_model.pkl", "rb"))
-vectorizer = pickle.load(open("vectorizers/tfidf_vectorizer.pkl", "rb"))
+vectorizer = pickle.load(open("vectorizers/tfidf_vectorizer3.pkl", "rb"))
 
-df = pd.read_csv('dataset/drugsComTrain_raw.csv')
-df_drug = df[(df['rating'] >= 9) & (df['usefulCount'] >= 100)].sort_values(by=['rating', 'usefulCount'], ascending=[False, False])
+df = pd.read_csv("dataset/drugsComTrain_raw.csv")
+df_drug = df[(df["rating"] >= 9) & (df["usefulCount"] >= 100)].sort_values(
+    by=["rating", "usefulCount"], ascending=[False, False]
+)
 
 
 def recommend_drug(disease):
-    recommended_drug_list = df_drug[df_drug['condition'] == disease]['drugName'].head(3).tolist()
+    recommended_drug_list = (
+        df_drug[df_drug["condition"] == disease]["drugName"].head(3).tolist()
+    )
     return recommended_drug_list
 
 
 def main():
     st.title("Disease Prediction")
 
+    st.sidebar.markdown(
+        "The **PassiveAggressiveClassifier** is a popular algorithm for online learning tasks and is often used for binary text classification problems. It is a type of linear classifier, suitable for large-scale learning. The PassiveAggressiveClassifier model, is trained on the TF-IDF transformed training data."
+    )
+    st.sidebar.markdown(
+        "Term Frequency-Inverse Document Frequency (TF-IDF). **TfidfVectorizer** is used to convert the text data into numerical features."
+    )
+    st.sidebar.markdown(
+        "**Dataset** includes 884 diseases, but the model is trained only on 11 most common diseases as to increase the accuracy and as this diseases contains highest instances in the dataset."
+    )
+    st.sidebar.markdown(
+        "Diseases on which model is trained are:\n"
+        "- Birth Control\n"
+        "- Depression\n"
+        "- Anxiety\n"
+        "- Acne\n"
+        "- Insomnia\n"
+        "- Diabetes, Type 2\n"
+        "- High Blood Pressure\n"
+        "- Migraine\n"
+        "- Constipation\n"
+        "- Cough\n"
+        "- GERD\n"
+    )
+
     with st.form(key="disease_clf_form"):
         raw_text = st.text_area("Type Your Symptoms Here")
         submit_text = st.form_submit_button(label="Submit")
 
     if submit_text:
-
         text_transformed = vectorizer.transform([raw_text])
         result = model.predict(text_transformed)[0]
         recommended_drug_list = recommend_drug(result)

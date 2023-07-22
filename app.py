f0d68249d8a915f69acc7cf6266b00d3aa4709ad
Pranav Patil <pranavpatil122002@gmail.com>
2023-07-22 23:35:07 +0530

    datase-zip

--- app.py
+++ app.py
@@ -2,11 +2,24 @@ import numpy as np
 import pickle
 import pandas as pd
 import streamlit as st
+import zipfile
+import os
+
+def unzip_file(zip_path, extract_to):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+
+
+zip_file_path = 'dataset.zip'  
+extraction_path = os.getcwd()  
+unzip_file(zip_file_path, extraction_path)
 
 model = pickle.load(open("models/tfidf_bigrams_model.pkl", "rb"))
 vectorizer = pickle.load(open("vectorizers/tfidf_vectorizer2.pkl", "rb"))
 
 df = pd.read_csv('dataset/drugsComTrain_raw.csv')
+
+
 df_drug = df[(df['rating'] >= 9) & (df['usefulCount'] >= 100)].sort_values(by=['rating', 'usefulCount'], ascending=[False, False])
 
 
